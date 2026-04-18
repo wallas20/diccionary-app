03a53f62d3b3cbad38a880cad5dbe3766fc16467
wallas20 <wasanchezd@unac.edu.pe>
2026-04-18 06:17:31 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,26 @@
-const CACHE = "diccionario-v1";
+const CACHE_NAME = "diccionario-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/css/index.css",
+  "/js/index.js",
+  "/manifest.json",
+  "/icono.png"
+];
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/css/index.css",
-        "/js/index.js"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
