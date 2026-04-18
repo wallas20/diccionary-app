acc6449644d9a92f477d66329dc62664c890dfb5
wallas20 <wasanchezd@unac.edu.pe>
2026-04-18 05:30:29 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,20 @@
-const CACHE_NAME = "diccionario-v1";
+const CACHE = "diccionario-v1";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/css/index.css",
-  "/js/index.js",
-  "/manifest.json"
-];
-
-// instalar y guardar archivos
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log("Cache creado");
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// activar (limpiar cache viejo si cambias versión)
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/css/index.css",
+        "/js/index.js"
+      ]);
     })
   );
 });
 
-// responder cuando no hay internet
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
